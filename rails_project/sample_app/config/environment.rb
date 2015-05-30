0ded769772eb2c8d42d5d8b745fcad1daecf9f73
Yoshimori Ito <ito@itandi.co.jp>
2015-05-30 15:20:08 +0900

    Finish user signup

--- rails_project/sample_app/config/environment.rb
+++ rails_project/sample_app/config/environment.rb
@@ -1,5 +1,10 @@
+SampleApp::Application.configure do
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
+
+# bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+  ActiveModel::SecurePassword.min_cost = true
+end
