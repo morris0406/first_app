f54ebddf6408adbe8c6898ce8b9688e47742efe3
Yoshimori Ito <ito@itandi.co.jp>
2015-06-13 16:39:28 +0900

    finish sign in

--- rails_project/sample_app/app/models/user.rb
+++ rails_project/sample_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+before_create :create_remember_token
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
@@ -10,4 +12,17 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def User.new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def User.encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+      def create_remember_token
+        self.remember_token = User.encrypt(User.new_remember_token)
+      end
 end
